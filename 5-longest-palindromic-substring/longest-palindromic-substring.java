54cc021f2a6e30a37f8c63acdc7a87f6db245579
Abhay Bhosle <97221697+abhayBhosle@users.noreply.github.com>
2023-09-18 21:28:13 +0530

    Time: 16 ms (77.34%) | Memory: 40.7 MB (98.00%) - LeetSync

---
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -0,0 +1,30 @@
+class Solution {
+    public String longestPalindrome(String s) {
+        int start = 0;
+        int end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandFromCenter(s, i, i);
+            // b a  b 
+            //   ij
+            int len2 = expandFromCenter(s, i, i + 1);
+            //  b a a b
+            //    i j
+
+            int len = Math.max(len1, len2);
+
+            if (len > end - start) { //if my desired (palindromer)substring is lesser than the len  than update start and end
+                start = i - (len - 1) / 2; //start index (if i am at ith position than for start i am going backward)
+                end = i + len / 2; //end index(and in this case i am moving forward)
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    int expandFromCenter(String s, int i, int j) {
+        while (i >= 0 && j < s.length() && s.charAt(i) == s.charAt(j)) { //while expanding dont go out of bound
+            i--;
+            j++;
+        }
+        return j - i - 1; // Corrected this line
+    }
+}
