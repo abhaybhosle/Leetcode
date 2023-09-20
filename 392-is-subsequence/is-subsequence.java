6a56b630b919da39e3bb4170fd07b61204da54e8
Abhay Bhosle <97221697+abhayBhosle@users.noreply.github.com>
2023-09-20 19:02:55 +0530

    Time: 2 ms (28.40%) | Memory: 40.2 MB (90.17%) - LeetSync

--- 392-is-subsequence/is-subsequence.java
+++ 392-is-subsequence/is-subsequence.java
@@ -1,14 +1,14 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
-        int si = 0;
+        int si = 0;//it will track the current index of String s
         if(s.length()<1){
-            return true;
+            return true;//as empty string(' ') is also considered as subsequence part
         }
         for(int i = 0; i < t.length();i++){
-            if(s.charAt(si) == t.charAt(i)){
+            if(s.charAt(si) == t.charAt(i)){//compare the char from s to the t , if they are same then increment si to next position,if not do nothing
                 si++;
             }
-            if(si == s.length()){
+            if(si == s.length()){ // if si is iterated over the String s, than check if it is equal to the length of String s , the return true;
                 return true;
             }
         }
