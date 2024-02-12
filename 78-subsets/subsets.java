99cf7da8d4a0399e6c467688e60f237ab8dd213d
Abhay Bhosle <97221697+abhaybhosle@users.noreply.github.com>
2024-02-12 17:40:21 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (8.13%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,30 +1,18 @@
 
-
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-         // Create a list to store all subsets
         List<List<Integer>> result = new ArrayList<>();
-
-        // Create a list to represent the current subset being considered
-        List<Integer> tempSet = new ArrayList<>();
-        
-        backtracking(result,tempSet,0,nums); //start backtracking from the beginning
+        backtrack(nums, 0, new ArrayList<>(), result);
         return result;
     }
-    private void backtracking(List<List<Integer>> result,List<Integer> tempSet,int start,int nums[]){
-        //add the set to the result set
-        result.add(new ArrayList<>(tempSet));
 
-        for(int i = start; i < nums.length;i++){
-            //case of including the number
-            tempSet.add(nums[i]);
+    private void backtrack(int nums[], int start, List<Integer> currSubset, List<List<Integer>> result) {
+        result.add(new ArrayList(currSubset));
 
-            //backtrack the new subset
-            backtracking(result,tempSet,i+1,nums);
-
-            //case of not including the number
-            tempSet.remove(tempSet.size()-1);
-
+        for (int i = start; i < nums.length; i++) {
+            currSubset.add(nums[i]);
+            backtrack(nums, i + 1, currSubset, result);
+            currSubset.remove(currSubset.size() - 1);
         }
     }
 }
