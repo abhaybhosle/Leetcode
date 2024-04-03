8a857c80f77b32937e9855ad39f6f64c1c9b3e4c
Abhay Bhosle <97221697+abhaybhosle@users.noreply.github.com>
2024-04-03 15:34:30 +0530

    Time: 26 ms (76.12%) | Memory: 67.8 MB (6.67%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.java
@@ -1,38 +1,32 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
-        int longestLength = 0;
-        Map<Integer,Boolean> exploredMap = new HashMap<>();
-        for(int num : nums){
-            exploredMap.put(num,Boolean.FALSE);
-        }
-        for(int num : nums){
-            int currentLength = 1;
-            //check in forward direction
-            int nextNum = num+1;
-            while(exploredMap.containsKey(nextNum) && exploredMap.get(nextNum) == false){
-                currentLength++;
-                exploredMap.put(nextNum,Boolean.TRUE);
+        HashSet<Integer> hs = new HashSet<>();
 
-                //Move to next number 
-                nextNum++;
-            }
-            //check in reverse direction
-            int prevNum = num-1;
-            while(exploredMap.containsKey(prevNum) && !exploredMap.get(prevNum)){
-                currentLength++;
-                exploredMap.put(prevNum,Boolean.TRUE);
-
-                //Move to next number 
-                prevNum--;
-            }
-            longestLength = Math.max(longestLength,currentLength);
+        for (int num : nums) {
+            hs.add(num);
         }
-        return longestLength;
+
+        int longestSeq = 0;
+
+        for (int num : hs) {
+            if (!hs.contains(num - 1)) {
+                int currNum = num;
+                int currSeq = 1;
+
+                while (hs.contains(currNum + 1)) {
+                    currNum++;
+                    currSeq++;
+                }
+                longestSeq = Math.max(currSeq, longestSeq);
+            }
+        }
+        return longestSeq;
     }
 }
 
 /*
-1) HashMap banao sab numbers ka aur sabko false rakha.
-2) Har ek number kae liye forward direction mae check kro , agar hae toh update kro varna 
-3) reverse mae check kro 
+ * 1) HashMap banao sab numbers ka aur sabko false rakha.
+ * 2) Har ek number kae liye forward direction mae check kro , agar hae toh
+ * update kro varna
+ * 3) reverse mae check kro
  */
