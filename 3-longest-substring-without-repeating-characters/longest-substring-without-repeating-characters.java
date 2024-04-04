a6d8f610e5698cb8521523723bc487e352c62da3
Abhay Bhosle <97221697+abhaybhosle@users.noreply.github.com>
2024-04-04 18:53:11 +0530

    Time: 6 ms (69.78%) | Memory: 44.4 MB (63.12%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -12,22 +12,34 @@
 
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        int left = 0;
-        Set<Character> set = new HashSet<>();
-        int maxLength = 0;
-
-        for(int right = 0; right < s.length(); right++){
-            if(!set.contains(s.charAt(right))){
-                set.add(s.charAt(right));
-                maxLength = Math.max(maxLength,right-left+1);
-            }else{
-                while(set.contains(s.charAt(right))){
-                    set.remove(s.charAt(left));
-                    left++;
-                }
-                set.add(s.charAt(right));
-            }
+        if (s == null || s.length() == 0) {
+            return 0;
         }
-        return maxLength;
+        Set<Character> set = new HashSet<>();
+        int maxLen = 0;
+        int left = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            char currChar = s.charAt(right);
+            while (set.contains(currChar)) {
+                set.remove(s.charAt(left));
+                left++;
+            }
+            set.add(currChar);
+            maxLen = Math.max(maxLen, right - left + 1);
+        }
+        return maxLen;
     }
 }
+/*
+ * At each step, the right pointer moves forward, and the characters encountered
+ * are added to a set.
+ * If a character is already present in the set, it indicates a repeating
+ * character. In this case, characters are removed from the set and the left
+ * pointer is incremented until the repeating character is no longer present in
+ * the set.
+ * Throughout this process, the length of the current substring without
+ * repeating characters is continuously updated.
+ * The maximum length encountered is tracked, providing the length of the
+ * longest substring without repeating characters in the given string s.
+ */
