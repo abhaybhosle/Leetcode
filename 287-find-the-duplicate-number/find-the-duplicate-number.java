9aaeb8f8091efe226e75f9bbe93bd86eb657a46d
Abhay Bhosle <97221697+abhaybhosle@users.noreply.github.com>
2025-06-01 16:41:15 +0530

    Time: 4 ms (89.06%) | Memory: 58.6 MB (31.13%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/find-the-duplicate-number.java
@@ -1,23 +1,18 @@
-class Solution{
-int findDuplicate(int[] nums) {
-    // Initialize slow and fast pointers
-    int slow = nums[0];
-    int fast = nums[0];
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int slow = nums[0];
+        int fast = nums[0];
 
-    // Step 1: Find the intersection point of the two pointers
-    do {
-        slow = nums[slow];
-        fast = nums[nums[fast]];
-    } while (slow != fast);
+        do{
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }while(slow != fast);
 
-    // Step 2: Find the entrance to the cycle
-    slow = nums[0];
-    while (slow != fast) {
+        slow = nums[0];
+        while (slow != fast) {
         slow = nums[slow];
         fast = nums[fast];
     }
-
-    // Return the repeated number
     return slow;
-}
+    }
 }
